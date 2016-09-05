731d5161aec1c859da4b4138f8765a390b5b485a
xiazhongda <xzd41@hotmail.com>
2016-09-05 20:36:00 +0800

    initla commit implemented bordering nodes table

---
+++ border/border.js
@@ -0,0 +1,183 @@
+var page_size = 20;
+var page_disp = 10;
+var start_page = 0;
+var active_page = 0;
+var date = "";
+
+var border_table = d3.select("#border_table");
+
+var xmlHttpRequest;
+
+var query_btn = d3.select("#query_btn")
+    .on("click",init_query_func);
+
+function is_date_time(date){
+    var reg = new RegExp("^2\\d{3}(0\\d|1[0-2])([0-2]\\d|3[0-1])$");
+    if (!reg.test(date)){
+        alert("invalid time format "+date+"(use YYYYMMDD)");
+        return false;
+    }
+    return true;
+}
+
+function init_query_func(){
+    var url = "query.php";
+    var date_text = d3.select("#date_text").property("value");
+    if ( ! is_date_time(date_text) ){
+        return;
+    }
+
+    date = date_text;
+
+    var post_str = "type=info&date="+date;
+
+    xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open("POST", url, true);
+    xmlHttpRequest.onreadystatechange = init_query_ready;
+    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xmlHttpRequest.send(post_str);
+}
+
+var dataset, table_list,num_page;
+
+function init_query_ready(){
+    if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+        var text = xmlHttpRequest.responseText;
+        var info_json = JSON.parse(text);
+        start_page = 0;
+        active_page = 0;
+        num_page = Math.ceil(info_json.num_node/page_size);
+
+        query_page();
+
+        $("#prev").click(function(){
+            start_page = 0;
+            active_page = 0;
+            query_page();
+        });
+        $("#next").click(function(){
+            start_page = num_page-page_disp;
+            active_page = num_page-1;
+            query_page();
+        });
+    }
+}
+
+function on_page_click(e){
+    var html = $(this).html();
+    var value = -1;
+    if (html == "..."){
+        value = start_page+page_disp;
+        start_page = value;
+    }
+    else if(html == ".."){
+        value = start_page-page_disp;
+        start_page = value;
+    }
+    else{
+        value= parseInt(html)-1;
+    }
+
+    active_page = value;
+    query_page();
+}
+
+function query_page(){
+    var start = (active_page)*page_size;
+    var end = (active_page+1)*page_size-1;
+    var url = "query.php";
+    var post_str = "type=page&start="+start+"&end="+end+"&date="+date;
+
+    xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open("POST", url, true);
+    xmlHttpRequest.onreadystatechange = refresh_nav;
+    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xmlHttpRequest.send(post_str);
+}
+    
+
+function refresh_nav(){
+    if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+    var text = xmlHttpRequest.responseText;
+    var page_json = JSON.parse(text);
+    table_list = page_json.nodes;
+
+    //remove remaining pages first.
+    var pages = $("#pages");
+    pages.find("li").not("#prev").not("#next").remove();
+
+    //make new page nav.
+    var prev = 0;
+    if(start_page >= page_disp){
+        pages.find("li").eq(0).after('<li class="item"><a>..</a></li>');
+        prev = 1;
+    }
+    for (var i=start_page; i<num_page && i<start_page+page_disp; i++){
+        if (i+1 > 0){
+            if (i!=active_page){
+                pages.find("li").eq(i-start_page+prev).after("<li class='item'><a>"+(i+1).toString()+"</a></li>");
+            }
+            else {
+                pages.find("li").eq(i-start_page+prev).after("<li class='item active'><a>" + (i + 1).toString() + "</a></li>");
+            }
+        }
+    }
+    if(start_page+page_disp < num_page) {
+        pages.find("li").eq(i-start_page+prev).after('<li class="item"><a>...</a></li>');
+    }
+    //add onclick listener.
+    pages.find("li").not("#prev").not("#next").find("a").click(on_page_click);
+
+    //clear table first.
+    var tbl = $("#border_table");
+    tbl.find("tbody tr").remove();
+
+    //make new table.
+    for (var j=0; j<table_list.length; j++) {
+        var row = tbl.find("tbody").append("<tr></tr>");
+        var cc;
+        if (table_list[j].geoip.mmdb.country == null || table_list[j].geoip.mmdb.country == "*"){
+            cc = "un";
+        }
+        else{
+            cc = table_list[j].geoip.mmdb.country.toLowerCase();
+        }
+        var num_neighbour = table_list[j].neighbour.length;
+        row.find("tr:eq("+ j.toString()+")").append('<td>'+ (active_page*page_size+j).toString()+'</td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td><span class="flag-icon flag-icon-' + cc + '"></span></td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td>' + table_list[j].addr + '</td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td>' + cc + '</td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td>' + table_list[j].geoip.bgp.asn + '</td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td>' + table_list[j].geoip.bgp.country + '</td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td>' + table_list[j].geoip.czdb.country + ','+ table_list[j].geoip.czdb.area +'</td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td>' + table_list[j].monitor + '</td>');
+        row.find("tr:eq("+ j.toString()+")").append('<td><a style=cursor:pointer onclick=neighbour_click('+j.toString()+')>' + num_neighbour + '</a></td>');
+    }
+    }
+}
+
+function neighbour_click(j){
+    var nbr_list = table_list[j].neighbour;
+
+    var tbl = $("#neighbour_table");
+    tbl.find("tbody tr").remove();
+    for (var i=0; i<nbr_list.length; i++) {
+        var row = tbl.find("tbody").append("<tr></tr>");
+        var cc;
+        if (nbr_list[i].geoip.mmdb.country == null || nbr_list[i].geoip.mmdb.country == "*"){
+            cc = "un";
+        }
+        else{
+            cc = nbr_list[i].geoip.mmdb.country.toLowerCase();
+        }
+        row.find("tr:eq("+ i.toString()+")").append('<td>'+ (i).toString()+'</td>');
+        row.find("tr:eq("+ i.toString()+")").append('<td><span class="flag-icon flag-icon-' + cc + '"></span></td>');
+        row.find("tr:eq("+ i.toString()+")").append('<td>' + nbr_list[i].addr + '</td>');
+        row.find("tr:eq("+ i.toString()+")").append('<td>' + cc + '</td>');
+        row.find("tr:eq("+ i.toString()+")").append('<td>' + nbr_list[i].geoip.bgp.asn + '</td>');
+        row.find("tr:eq("+ i.toString()+")").append('<td>' + nbr_list[i].geoip.bgp.country + '</td>');
+        row.find("tr:eq("+ i.toString()+")").append('<td>' + nbr_list[i].geoip.czdb.country + ','+ nbr_list[i].geoip.czdb.area +'neighbour(s)</td>');
+    }
+
+    $('#neighbour_modal').modal();
+}
---
+++ border/query.php
@@ -0,0 +1,76 @@
+<?php 
+	$config = parse_ini_file("config.ini");
+	$servername = $config["servername"];
+	$username = $config["username"];
+	$password = $config["password"];
+	$database = $config["database"];
+
+	if ( !empty($_POST["type"]) && !empty($_POST["date"]) ){
+		$date = $_POST["date"];
+		$type = $_POST["type"];
+		$con = mysql_connect($servername, $username, $password);
+		if (! $con){
+			die('Could not connect:'.mysql_error());
+		}
+		mysql_select_db($database, $con);
+		if ($type == "info") {
+			$sql = "SELECT count(id) as num_node from "."".$date.""."_border_tbl";
+			$result = mysql_query($sql);
+			$count = -1;
+			while( $row = mysql_fetch_array(($result)) ){
+				$count = (int)$row["num_node"];
+			}
+			$res = array(
+				"num_node" => $count
+				);
+			echo json_encode($res);
+		}
+		elseif ($type == "page") {
+			$sql = "";
+			$start = $_POST["start"];
+			$end = $_POST["end"];
+			$num = $end-$start+1;
+			$sql = "SELECT * from "."".$date.""."_border_tbl limit "."".$start."".","."".$num;
+			//echo $sql."\n";
+			$result = mysql_query($sql);
+			$res = array();
+			$foreign_neighbours = "";
+			$sql_node_pfx = "SELECT node.id as id, node.addr as addr, node.monitor as monitor, geoip.geoip as geoip FROM "."".$date.""."_node_tbl as node, "."".$date.""."_geoip_tbl as geoip where node.id=geoip.id and node.id=";
+			while ( $row = mysql_fetch_array($result) ){
+				$id = $row["id"];
+				$sql_node = $sql_node_pfx.$id;
+				$node_result = mysql_query($sql_node);
+				$node_row = mysql_fetch_array($node_result);
+
+				$foreign_neighbours = $row["foreign_neighbours"];
+				$list = explode("|", $foreign_neighbours);
+				$foreign_neighbours_list = array();
+
+				foreach($list as $nbr){
+					$sql_nbr = $sql_node_pfx.$nbr;
+					//echo $sql_node."\n";
+					$nbr_result = mysql_query($sql_nbr);
+					$nbr_row = mysql_fetch_array($nbr_result);
+					$res_nbr = array(
+						"id" => $nbr_row["id"],
+						"addr" => $nbr_row["addr"],
+						"monitor" => $nbr_row["monitor"],
+						"geoip" => json_decode($nbr_row["geoip"])
+						);
+					array_push($foreign_neighbours_list, $res_nbr);
+				}
+
+				$res_node = array(
+					"id" => $node_row["id"],
+					"addr" => $node_row["addr"],
+					"monitor" => $node_row["monitor"],
+					"geoip" => json_decode($node_row["geoip"]),
+					"neighbour" => $foreign_neighbours_list
+					);
+
+				array_push($res, $res_node);
+			}
+			echo json_encode(array("nodes" => $res));
+		}
+	}
+?>
